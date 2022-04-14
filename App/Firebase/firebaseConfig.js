5699c3b10c89d00c508f507263c41fc51148a570
“Raheemshah2809” <Raheemshah2809@gmail.com>
2022-04-14 16:05:23 +0100

    initial commit

---
+++ App/Firebase/firebaseConfig.js
@@ -0,0 +1,14 @@
+import Firebase from 'firebase';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6gbg8fdHTDlZbFv4AOaCm4zDCEXMrJ0M",
+  authDomain: "thechatter-ec861.firebaseapp.com",
+  projectId: "thechatter-ec861",
+  storageBucket: "thechatter-ec861.appspot.com",
+  messagingSenderId: "128478114918",
+  appId: "1:128478114918:web:6c02753cc7d6e080d229a7"
+};
+
+
+export default Firebase.initializeApp(firebaseConfig);
